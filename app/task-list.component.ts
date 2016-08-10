b0578904ac68765b0b72057a45b1988063ddbf44
Sam Park and Tony Fabery <tony@>
2016-08-10 16:12:57 -0700

    add NewTaskComponent

--- app/task-list.component.ts
+++ app/task-list.component.ts
@@ -2,13 +2,14 @@ import { Component, EventEmitter } from 'angular2/core';
 import { TaskComponent } from './task.component';
 import { Task } from './task.model';
 import { EditTaskDetailsComponent } from './edit-task-details.component';
+import { NewTaskComponent } from './new-task.component';
 
 @Component({
   selector: 'task-list',
   inputs: ['taskList'],
   outputs: ['onTaskSelect'],
-  directives: [TaskComponent, EditTaskDetailsComponent],
-  templateUrl: `app/task-list.component.html`
+  directives: [TaskComponent, EditTaskDetailsComponent, NewTaskComponent],
+  templateUrl: 'app/task-list.component.html'
 })
 
 export class TaskListComponent {
@@ -18,10 +19,17 @@ export class TaskListComponent {
   constructor() {
     this.onTaskSelect = new EventEmitter();
   }
+
   taskClicked(clickedTask: Task): void {
     console.log('child', clickedTask);
     this.selectedTask = clickedTask;
     this.onTaskSelect.emit(clickedTask);
     ///line #29 gives you the parent of this method taskWasSelected that runs the line taskWasSelected(clickedTask: Task): void {console.log('parent', clickedTask);
+  }
+
+  createTask(description: string): void {
+    this.taskList.push(
+      new Task(description, this.taskList.length)
+    );
   }
 }
