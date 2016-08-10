688b4f68d3ee09ee70433b92da70e0dc55f12465
Sam Park and Tony Fabery <sam@>
2016-08-10 15:44:26 -0700

    add EditTaskDetailsComponent

---
+++ app/task.component.ts
@@ -0,0 +1,12 @@
+import { Component } from 'angular2/core';
+import { Task } from './task.model';
+
+@Component({
+    selector: 'task-display',
+    inputs: ['task'],
+  templateUrl: `app/task.component.html`
+})
+
+export class TaskComponent {
+  public task: Task;
+}
